10155e388e4146fe9735c0f87d872b201ea6c8a2
Ilhom-Utkirov <Inspiration9595@gmail.com>
2021-01-25 17:35:20 +0500

    project started successfully

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-        System.out.println("Poject started successfully");
+        System.out.println("Project started successfully");
     }
 
 }
